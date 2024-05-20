84ca6b612b6b2d52128622a051262aa5e0669f45
Valentin <sighwayve@gmail.com>
2024-05-20 16:23:27 +0200

    variable correctted on connection/dotenv on app.js

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://sighwayve:G5ydPgH35v2fNq7G@sgwvlacapsule.6ecjitm.mongodb.net/gamecho';
+const connectionString = process.env.CONNECTION_STRING
 
 // don't forget to use your own connectionString to exploit the database
 // set it as an env.
