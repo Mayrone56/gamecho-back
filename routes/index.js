683a55a844766afa058c338a37414efa731ef7fe
Valentin <sighwayve@gmail.com>
2024-05-20 09:49:49 +0200

    setup

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+const bcrypt = require('bcrypt');
+const hash = bcrypt.hashSync('password', 10);
+const uid2 = require('uid2');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
