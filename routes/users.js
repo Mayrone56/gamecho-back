33e6928f02e2fcc1127c219a74ed1be3db6f5484
Gtfoohfs <karl.hosni@icloud.com>
2024-05-21 15:05:41 +0200

    fetch api

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 require('../models/connection');
 
-const User = require('../models/user');
+const User = require('../models/users');
 
 
 const { checkBody } = require('../modules/checkBody');
