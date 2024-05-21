bc82cd397ae42f1f492bd9a7781b4cf59243c73a
Valentin <sighwayve@gmail.com>
2024-05-21 17:11:56 +0200

    fileupload/profile route/tmp

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-require('dotenv').config();
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var profileRouter = require("./routes/profile");
 
 var app = express();
-const cors = require('cors');
+
+const fileUpload = require("express-fileupload");
+app.use(fileUpload());
+const cors = require("cors");
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/profile", profileRouter);
 
 module.exports = app;
