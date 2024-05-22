0ed4f481e8ef2feb5c209e215e3929ec6436fdf4
Gtfoohfs <karl.hosni@icloud.com>
2024-05-22 18:08:55 +0200

    modif route.get

--- routes/games.js
+++ routes/games.js
@@ -6,32 +6,27 @@ const User = require('../models/users')
 
 const API_KEY = process.env.API_KEY
 
-// router.get('/', (req, res) => {
-//   fetch(`https://api.rawg.io/api/games?key=${API_KEY}`)
-//     .then(response => response.json())
-//     .then(data => {
-//       if (data) {
-//         const formatedGame = {
-//           name: data.name,
-//           image: data.background_image
-
-//         }
-//         res.json({ result: true, games: data.results })
-//       } else {
-//         res.json({ games: [] })
-//       }
-//     });
-// });
+router.get('/', (req, res) => {
+  fetch(`https://api.rawg.io/api/games?key=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      if (data) {
+        res.json({ result: true, games: data.results })
+      } else {
+        res.json({ games: [] })
+      }
+    });
+});
 
 
 // Cette route servira à rajouter des jeux à notre wishlist
 // A TERMINER!!!!!!! API???
 router.post('/games', (req, res) => {
   // Nous vérifions tout d'abord si le jeu existe ou non dans notre database
-  Game.findOne({ gameName: { $regex: new RegExp(req.body.gameName, 'i') } }).then(MongoData => {
+  Game.findOne({ name: { $regex: new RegExp(req.body.gameName, 'i') } }).then(MongoData => {
     if (MongoData === null) {
       // Si le jeu n'est pas trouvé dans notre base de donnée, on va récupérer l'info grâce à une API
-      fetch(`https://api.rawg.io/api/{req.body.gameName}?key=${API_KEY}`)
+      fetch(`https://api.rawg.io/api/{req.body.name}?key=${API_KEY}`)
         .then(response => response.json())
         // Attention à cette étape, nous stockons notre jeu sous forme d'objet correspondant
         // aux couples clés/valeurs de notre modèle "games"
